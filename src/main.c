6e38e3afd3daace3a42e09395041854f9ad5bf56
Vladislav <vladkhmarenko@icloud.com>
2024-04-03 15:31:20 +0000

    task completed

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-/* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int countLetter(char* sentence, char letter) {
+    int count = 0;
+    int i = 0;
+    while (sentence[i] != '\0') {
+        if (sentence[i] == letter) {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+int main() {
+    char sentence[100];
+    char letter;
+
+    printf("Введите предложение: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    printf("Введите букву: ");
+    scanf("%c", &letter);
+
+    int letterCount = countLetter(sentence, letter);
+
+    printf("Количество появлений буквы '%c' в предложении: %d\n", letter, letterCount);
+
+    return 0;
 }
